29035884bc533df65b0418a1d1664fbf3a17e736
Wisam Rabban <wrabban@gmail.com>
2010-11-08 21:40:44 -0800

    Include MATLAB and Arudino libraries necessary for serial communication between MATLAB and Arduino

---
+++ ArduinoIO/ArduinoIO/contents.m
@@ -0,0 +1,23 @@
+% Arduino IO Package Class
+% Version 2.3 (R2010a), G. Campa,  06-Jun-2010
+% 
+% This is a MATLAB class that allows performing Analog/Digital Input 
+% and Output with the Arduino Board from the MATLAB command line.
+% Please read the file readme.txt for more info
+%
+% Files:
+% 
+% Readme.txt              : file you should read before doing anything
+% contents.m              : this file (needed for matlab help)
+% 
+% arduino.m               : defines the arduino class
+% 
+% adiosrv                 : folder containing adiosrv.pde sketch
+% motorsrv                : folder containing motorsrv.pde sketch
+% 
+% examples                : folder containing a few examples, specifically:
+% blink_challenge.m       : blink challenge code
+% blinkchallenge.mdl      : blink challenge schematics
+% example_io.m            : IO example
+% example_mot.m           : motor example
+% 
